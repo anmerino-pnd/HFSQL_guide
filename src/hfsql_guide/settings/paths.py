6b1a44d64b1cfd517b44e6975df792137067e63d
Angel Merino <acedeno00@gmail.com>
2025-11-17 17:03:10 -0700

    name changed

--- src/hfsql_guide/settings/paths.py
+++ src/hfsql_guide/settings/paths.py
@@ -11,6 +11,8 @@ BASE_DIR = find_project_root(Path(__file__))
 
 DATA_DIR = BASE_DIR / "data"
 PARQUET_DIR = DATA_DIR / "parquet" 
+FAILED_TABLES = DATA_DIR / "failed_tables.txt"
+BLACKLISTED_TABLES = DATA_DIR / "blacklist.txt"
 
 
 for path in [
@@ -18,3 +20,8 @@ for path in [
     PARQUET_DIR
              ]:
     path.mkdir(parents=True, exist_ok=True)
+
+for file in [
+    FAILED_TABLES, 
+    BLACKLISTED_TABLES]:
+    file.touch(exist_ok=True)
